6756c97496a34f329ab9d1aa6e1fb145b3b0558e
Eden1111 <128496611+Eden1011@users.noreply.github.com>
2024-01-28 12:11:42 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def suma(a,b):
+	return a + b
+def test_suma():
+	a = 2
+	b = 4
+	assert suma(a,b) == a + b
+
+ 
+def iloczyn(a,b):
+        return a * b
+def test_iloczyn():
+        a = 2
+        b = 4
+        assert iloczyn(a,b) == a * b
+
